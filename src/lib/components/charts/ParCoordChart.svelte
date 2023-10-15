637d30e317d26bb53e82df13b29968434830176f
jjones <jimmy.jones@tutanota.com>
2023-10-14 19:38:07 -0600

    create chart component

---
+++ src/lib/components/charts/ParCoordChart.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let data: any;
+</script>
+
+{JSON.stringify(data.data.default)}
+{JSON.stringify(data.keys)}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+	import ParCoordChart from '$lib/components/charts/ParCoordChart.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-{JSON.stringify(data.data.default)}
-{JSON.stringify(data.keys)}
+<ParCoordChart {data} />
